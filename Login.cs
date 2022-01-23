0cc87c10b47e5abea6623e373a251e5c4d8a08c1
Siniša Vešligaj <svesligaj@tvz.hr>
2022-01-23 15:18:56 +0100

    Dataset and login

--- Login.cs
+++ Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace NTP_Projekt
 {
@@ -20,6 +21,29 @@ namespace NTP_Projekt
         private void btnLogin_Click(object sender, EventArgs e)
         {
             this.Hide();
+            string path = @"Data Source=DESKTOP-I5DQ78V;Initial Catalog=ntp_projekt;Integrated Security=True";
+            
+            SqlConnection conn;
+            conn = new SqlConnection(path);
+
+            conn.Open();
+            SqlCommand cmd;
+            SqlDataReader reader;
+            String sql = "SELECT * FROM Users WHERE RoleID = 3";
+            String output = "";
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                output = output + reader.GetString(0) +  " - " + reader.GetString(1);
+            }
+            MessageBox.Show(output);           
+
+            reader.Close();
+            cmd.Dispose();
+            conn.Close();   
+
             Main mn = new Main();
             mn.Show();
         }
