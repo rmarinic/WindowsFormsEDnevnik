f01679a5dc8cd1b7fc5f22814cce0ffe1b2d3797
Siniša Vešligaj <svesligaj@tvz.hr>
2022-02-08 13:30:39 +0100

    WIP for threads

--- View/ProfMain.Designer.cs
+++ View/ProfMain.Designer.cs
@@ -29,12 +29,68 @@ namespace NTP_Projekt.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.GradeAddPnl = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Student list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add a grade";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // GradeAddPnl
+            // 
+            this.GradeAddPnl.Location = new System.Drawing.Point(1001, 41);
+            this.GradeAddPnl.Name = "GradeAddPnl";
+            this.GradeAddPnl.Size = new System.Drawing.Size(377, 645);
+            this.GradeAddPnl.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(983, 645);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ProfMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1387, 696);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.GradeAddPnl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ProfMain";
             this.Text = "ProfMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel GradeAddPnl;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- View/ProfMain.cs
+++ View/ProfMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NTP_Projekt.Logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,86 @@ namespace NTP_Projekt.View
         public ProfMain()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fetch_enrolled_students()
+        {
+            using(ntp_projektEntities1 db = new ntp_projektEntities1())
+            {
+                var jmbag = Globals.USER_JMBAG;
+                var professor = db.Professors.SingleOrDefault(p => p.JMBAG == jmbag);
+                var query = from user in db.Users
+                            join students in db.Students
+                            on user.JMBAG equals students.JMBAG
+                            join enrollments in db.Enrollments
+                            on students.JMBAG equals enrollments.StudentJMBAG
+                            join courses in db.Courses
+                            on enrollments.CourseID equals courses.CourseID
+                            join grades in db.Grades
+                            on courses.CourseID equals grades.CourseID
+                            where user.RoleID == 1 && enrollments.CourseID == professor.CourseID
+                            select new
+                            {
+                                user.JMBAG,
+                                user.FirstName,
+                                user.LastName,
+                                user.Email
+                            };
+                dataGridView1.DataSource = query.ToList();
+                add_columns_to_view();
+                foreach(DataGridViewRow row in dataGridView1.Rows)
+                {
+                    string stud_jmbag = row.Cells[0].Value.ToString();
+                    var first_exam_grade = do_tasks(stud_jmbag, "first_exam");
+                    Console.WriteLine(first_exam_grade);
+                    //do_tasks(stud_jmbag, "second_exam");
+                }
+            }
+        }
+
+        public Grades do_tasks(string jmbag, string type)
+        {
+            Task<Grades> task = Task<Grades>.Factory.StartNew(() =>
+            {
+                using(ntp_projektEntities1 db = new ntp_projektEntities1())
+                {
+                    var grade = from grades in db.Grades
+                                where grades.StudentJMBAG == jmbag && grades.Type == type
+                                orderby grades.Score
+                                select grades;
+                    return (Grades)grade;
+                }
+            });
+            return task.Result;
+        }
+
+        private void add_columns_to_view()
+        {
+            DataGridViewColumn first_exam = new DataGridViewColumn();
+            DataGridViewColumn second_exam = new DataGridViewColumn();
+            DataGridViewColumn third_exam = new DataGridViewColumn();
+            DataGridViewColumn practical_exercise = new DataGridViewColumn();
+            first_exam.Name = "firstExam";
+            first_exam.HeaderText = "First Exam";
+            first_exam.CellTemplate = new DataGridViewTextBoxCell();
+            dataGridView1.Columns.Insert(4, first_exam);
+            second_exam.Name = "secondExam";
+            second_exam.HeaderText = "Second Exam";
+            second_exam.CellTemplate = new DataGridViewTextBoxCell();
+            dataGridView1.Columns.Insert(5, second_exam);
+            third_exam.Name = "thirdExam";
+            third_exam.HeaderText = "Third Exam";
+            third_exam.CellTemplate = new DataGridViewTextBoxCell();
+            dataGridView1.Columns.Insert(6, third_exam);
+            practical_exercise.Name = "pracExercise";
+            practical_exercise.HeaderText = "Practical Exercise";
+            practical_exercise.CellTemplate = new DataGridViewTextBoxCell();
+            dataGridView1.Columns.Insert(7, practical_exercise);
         }
     }
 }
