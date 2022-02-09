1b65cb0f245799b5dc1da78ba3bde9e6aecc3906
Siniša Vešligaj <svesligaj@tvz.hr>
2022-02-09 01:38:04 +0100

    Finish Professor view

--- View/ProfMain.Designer.cs
+++ View/ProfMain.Designer.cs
@@ -33,6 +33,18 @@ namespace NTP_Projekt.View
             this.button2 = new System.Windows.Forms.Button();
             this.GradeAddPnl = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.GradeAddPnl.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,9 +66,20 @@ namespace NTP_Projekt.View
             this.button2.TabIndex = 1;
             this.button2.Text = "Add a grade";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // GradeAddPnl
             // 
+            this.GradeAddPnl.Controls.Add(this.ID);
+            this.GradeAddPnl.Controls.Add(this.textBox3);
+            this.GradeAddPnl.Controls.Add(this.label3);
+            this.GradeAddPnl.Controls.Add(this.textBox2);
+            this.GradeAddPnl.Controls.Add(this.label2);
+            this.GradeAddPnl.Controls.Add(this.comboBox1);
+            this.GradeAddPnl.Controls.Add(this.label1);
+            this.GradeAddPnl.Controls.Add(this.textBox1);
+            this.GradeAddPnl.Controls.Add(this.button4);
+            this.GradeAddPnl.Controls.Add(this.button3);
             this.GradeAddPnl.Location = new System.Drawing.Point(1001, 41);
             this.GradeAddPnl.Name = "GradeAddPnl";
             this.GradeAddPnl.Size = new System.Drawing.Size(377, 645);
@@ -69,18 +92,117 @@ namespace NTP_Projekt.View
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(983, 645);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(91, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "JMBAG";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(174, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Exam Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Score";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(79, 107);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 9;
+            this.ID.Text = "ID";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(174, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(294, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(135, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Add a student";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // ProfMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1387, 696);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.GradeAddPnl);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "ProfMain";
             this.Text = "ProfMain";
+            this.GradeAddPnl.ResumeLayout(false);
+            this.GradeAddPnl.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -92,5 +214,16 @@ namespace NTP_Projekt.View
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel GradeAddPnl;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
     }
 }
--- View/ProfMain.cs
+++ View/ProfMain.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,17 +17,91 @@ namespace NTP_Projekt.View
         public ProfMain()
         {
             InitializeComponent();
+            GradeAddPnl.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            dataGridView1.DataSource = null;
+            fetch_enrolled_students();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GradeAddPnl.Visible = true;
+            fill_combobox();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            GradeAddPnl.Visible = false;
+            fetch_unenrolled_students();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (ntp_projektEntities1 db = new ntp_projektEntities1())
+            {
+                var jmbag = Globals.USER_JMBAG;
+                var professor = db.Professors.SingleOrDefault(p => p.JMBAG == jmbag);
+                string stud_jmbag = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                textBox1.Text = stud_jmbag;
+                var exam_type = resolve_exam_type(comboBox1.SelectedItem.ToString());
+                var grade = db.Grades.SingleOrDefault(g => g.CourseID == professor.CourseID && g.Type == exam_type && g.StudentJMBAG == stud_jmbag);
+                if (grade != null)
+                {
+                    grade.Score = Convert.ToInt32(textBox2.Text);
+                    db.SaveChanges();
+                    fetch_enrolled_students();
+                }
+                else
+                {
+                    Grades new_grade = new Grades();
+                    new_grade.ID = Convert.ToInt32(textBox3.Text);
+                    new_grade.StudentJMBAG = stud_jmbag;
+                    new_grade.Score = Convert.ToInt32(textBox2.Text);
+                    new_grade.CourseID = professor.CourseID;
+                    new_grade.Type = exam_type;
+                    db.Grades.Add(new_grade);
+                    db.SaveChanges();
+                    fetch_enrolled_students();
+                }
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (GradeAddPnl.Visible == true)
+            {
+                textBox1.Text = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                int column_index = this.dataGridView1.CurrentCell.ColumnIndex;
+                comboBox1.SelectedItem = dataGridView1.Columns[column_index].HeaderText;
+                textBox2.Text = this.dataGridView1.CurrentCell.Value.ToString();
+                textBox1.ReadOnly = true;
+                button3.Text = "Update";
+            }
+            else
+            {
+                ntp_projektEntities1 db = new ntp_projektEntities1();
+                var jmbag = Globals.USER_JMBAG;
+                var professor = db.Professors.SingleOrDefault(p => p.JMBAG == jmbag);
+                Random rand = new Random();
+                int id = rand.Next(5, 1000);
+                Enrollments enrollment = new Enrollments();
+                enrollment.EnrollmentID = id;
+                enrollment.CourseID = (int)professor.CourseID;
+                enrollment.StudentJMBAG = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                fetch_unenrolled_students();
+            }
         }
 
         private void fetch_enrolled_students()
         {
             using(ntp_projektEntities1 db = new ntp_projektEntities1())
             {
+                dataGridView1.Columns.Clear();
                 var jmbag = Globals.USER_JMBAG;
                 var professor = db.Professors.SingleOrDefault(p => p.JMBAG == jmbag);
                 var query = from user in db.Users
@@ -36,8 +111,6 @@ namespace NTP_Projekt.View
                             on students.JMBAG equals enrollments.StudentJMBAG
                             join courses in db.Courses
                             on enrollments.CourseID equals courses.CourseID
-                            join grades in db.Grades
-                            on courses.CourseID equals grades.CourseID
                             where user.RoleID == 1 && enrollments.CourseID == professor.CourseID
                             select new
                             {
@@ -47,31 +120,79 @@ namespace NTP_Projekt.View
                                 user.Email
                             };
                 dataGridView1.DataSource = query.ToList();
-                add_columns_to_view();
-                foreach(DataGridViewRow row in dataGridView1.Rows)
+                if (dataGridView1.ColumnCount == 4)
+                    add_columns_to_view();
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
                 {
                     string stud_jmbag = row.Cells[0].Value.ToString();
-                    var first_exam_grade = do_tasks(stud_jmbag, "first_exam");
-                    Console.WriteLine(first_exam_grade);
-                    //do_tasks(stud_jmbag, "second_exam");
+                    int first_exam_grade = do_tasks(stud_jmbag, "first_exam");
+                    int second_exam_grade = do_tasks(stud_jmbag, "second_exam");
+                    int third_exam_grade = do_tasks(stud_jmbag, "third_exam");
+                    int practical_exam_grade = do_tasks(stud_jmbag, "practical_exercise");
+                    row.Cells[4].Value = first_exam_grade;
+                    row.Cells[5].Value = second_exam_grade;
+                    row.Cells[6].Value = third_exam_grade;
+                    row.Cells[7].Value = practical_exam_grade;
                 }
+
             }
         }
 
-        public Grades do_tasks(string jmbag, string type)
+        private void fetch_unenrolled_students()
         {
-            Task<Grades> task = Task<Grades>.Factory.StartNew(() =>
+            using (ntp_projektEntities1 db = new ntp_projektEntities1())
             {
-                using(ntp_projektEntities1 db = new ntp_projektEntities1())
+                dataGridView1.Columns.Clear();
+                var jmbag = Globals.USER_JMBAG;
+                var professor = db.Professors.SingleOrDefault(p => p.JMBAG == jmbag);
+                var query = from user in db.Users
+                            join students in db.Students
+                            on user.JMBAG equals students.JMBAG
+                            join enrollments in db.Enrollments
+                            on students.JMBAG equals enrollments.StudentJMBAG into enroll
+                            from enrollment in enroll.DefaultIfEmpty()
+                            where user.RoleID == 1 && enrollment.CourseID != professor.CourseID
+                            select new
+                            {
+                                user.JMBAG,
+                                user.FirstName,
+                                user.LastName,
+                                user.Email
+                            };
+                dataGridView1.DataSource = query.ToList();
+            }
+
+        }
+
+        public int do_tasks(string jmbag, string type)
+        {
+            using (ntp_projektEntities1 db = new ntp_projektEntities1())
+            {
+                Task<int> task = Task<int>.Factory.StartNew(() =>
                 {
                     var grade = from grades in db.Grades
                                 where grades.StudentJMBAG == jmbag && grades.Type == type
                                 orderby grades.Score
-                                select grades;
-                    return (Grades)grade;
-                }
-            });
-            return task.Result;
+                                select new
+                                {
+                                    grades.Score
+                                };
+
+                    bool has_score = grade.Any(c => c.Score != 0);
+                    if (has_score)
+                    {
+                        var best_grade = grade.ToList().First();
+                        Console.WriteLine("Thread #{0} with the score {1} and type {2}", Thread.CurrentThread.ManagedThreadId, best_grade.Score, type);
+                        return best_grade.Score;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                });
+                return task.Result;
+            }
         }
 
         private void add_columns_to_view()
@@ -81,21 +202,60 @@ namespace NTP_Projekt.View
             DataGridViewColumn third_exam = new DataGridViewColumn();
             DataGridViewColumn practical_exercise = new DataGridViewColumn();
             first_exam.Name = "firstExam";
-            first_exam.HeaderText = "First Exam";
+            first_exam.HeaderText = "First exam";
             first_exam.CellTemplate = new DataGridViewTextBoxCell();
             dataGridView1.Columns.Insert(4, first_exam);
             second_exam.Name = "secondExam";
-            second_exam.HeaderText = "Second Exam";
+            second_exam.HeaderText = "Second exam";
             second_exam.CellTemplate = new DataGridViewTextBoxCell();
             dataGridView1.Columns.Insert(5, second_exam);
             third_exam.Name = "thirdExam";
-            third_exam.HeaderText = "Third Exam";
+            third_exam.HeaderText = "Third exam";
             third_exam.CellTemplate = new DataGridViewTextBoxCell();
             dataGridView1.Columns.Insert(6, third_exam);
             practical_exercise.Name = "pracExercise";
-            practical_exercise.HeaderText = "Practical Exercise";
+            practical_exercise.HeaderText = "Practical exercises";
             practical_exercise.CellTemplate = new DataGridViewTextBoxCell();
             dataGridView1.Columns.Insert(7, practical_exercise);
+        }
+
+        private void fill_combobox()
+        {
+            comboBox1.Items.Add("First exam");
+            comboBox1.Items.Add("Second exam");
+            comboBox1.Items.Add("Third exam");
+            comboBox1.Items.Add("Practical exercises");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private string resolve_exam_type(string type)
+        {
+            string exam_type;
+            switch (type)
+            {
+                case "First exam":
+                    exam_type = "first_exam";
+                    break;
+                case "Second exam":
+                    exam_type = "second_exam";
+                    break;
+                case "Third exam":
+                    exam_type = "third_exam";
+                    break;
+                default:
+                    exam_type = "practical_exercise";
+                    break;
+            }
+            return exam_type;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = false;
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            comboBox1.SelectedIndex = 0;
         }
     }
 }
